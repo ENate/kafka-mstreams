ee80bce69db882000e026d27e87d3504fb3d2c65
Nathaniel Egwu <nathaniel@aims.ac.za>
2023-02-14 00:21:06 +0000

    Updated edge and discovery servers

--- composite-service/src/main/java/com/minejava/kafkastreams/compositeservice/config/SecurityConfig.java
+++ composite-service/src/main/java/com/minejava/kafkastreams/compositeservice/config/SecurityConfig.java
@@ -1,27 +1,23 @@
-package com.minejava.kafkastreams.edgeservice.config;
+package com.minejava.kafkastreams.compositeservice.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-
 @Configuration
 @EnableWebFluxSecurity
-public class EdgeConfigSecurity {
+public class SecurityConfig {
 
 
-    @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 
         http
             .csrf(csrf -> csrf.disable())
-            .authorizeExchange(exchange -> exchange
+            .authorizeExchange(exch -> exch
                 .pathMatchers("/users").permitAll()
                 .anyExchange().authenticated());
         return http.build();
-        
     }
     
 }
